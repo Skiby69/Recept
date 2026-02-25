b24ccc84e6ba4b09ff1d0ed7ac44a2dbcc7170df
Skiby <karsai.marko@gmail.com>
2026-02-25 13:27:30 +0100

    majkrem

---
+++ script.js
@@ -0,0 +1,86 @@
+const receptek = [
+    {
+        nev: "Palacsinta",
+        hozzavalok: ["tojás", "liszt", "tej", "cukor"]
+    },
+    {
+        nev: "Rántotta",
+        hozzavalok: ["tojás", "sajt"]
+    },
+    {
+        nev: "Sajtos paradicsomos omlett",
+        hozzavalok: ["tojás", "sajt", "paradicsom"]
+    },
+    {
+        nev: "Grízes tészta",
+        hozzavalok: ["liszt", "cukor", "vaj"]
+    },
+    {
+        nev: "Paradicsomos tészta",
+        hozzavalok: ["liszt", "paradicsom", "sajt"]
+    },
+    {
+        nev: "Sajtos melegszendvics",
+        hozzavalok: ["sajt", "vaj"]
+    },
+    {
+        nev: "Túrós palacsinta",
+        hozzavalok: ["tojás", "liszt", "tej", "cukor", "túró"]
+    },
+    {
+        nev: "Csirkés rizs",
+        hozzavalok: ["csirke", "rizs", "só"]
+    },
+    {
+        nev: "Sült krumpli",
+        hozzavalok: ["krumpli", "só", "olaj"]
+    },
+    {
+        nev: "Rizses hús",
+        hozzavalok: ["csirke", "rizs", "só", "olaj"]
+    },
+    {
+        nev: "Krumplipüré",
+        hozzavalok: ["krumpli", "vaj", "tej", "só"]
+    },
+    {
+        nev: "Sajtos tészta",
+        hozzavalok: ["liszt", "sajt", "vaj"]
+    },
+    {
+        nev: "Tojásos nokedli",
+        hozzavalok: ["tojás", "liszt", "só"]
+    },
+    {
+        nev: "Bundás kenyér",
+        hozzavalok: ["tojás", "tej", "só", "olaj"]
+    },
+    {
+        nev: "Tejeskávé",
+        hozzavalok: ["tej", "cukor"]
+    },
+    {
+        nev: "Csirkepörkölt",
+        hozzavalok: ["csirke", "olaj", "só", "paradicsom"]
+    },
+    {
+        nev: "Sajtos krumpli",
+        hozzavalok: ["krumpli", "sajt", "vaj"]
+    },
+    {
+        nev: "Paradicsomleves",
+        hozzavalok: ["paradicsom", "só", "cukor"]
+    },
+    {
+        nev: "Rizsfelfújt",
+        hozzavalok: ["rizs", "tej", "cukor", "tojás"]
+    },
+    {
+        nev: "Túrós csusza",
+        hozzavalok: ["liszt", "túró", "tejföl", "só"]
+    },
+    {
+        nev: "Sajtos omlett",
+        hozzavalok: ["tojás", "sajt", "só"]
+    }
+];
